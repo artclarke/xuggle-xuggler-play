1b58d7800c9b0bcbe65e88d737e8577136f9d82a
Ryan Brainard <brainard@heroku.com>
2012-03-12 23:05:43 -0700

    rename Application to Instructions

--- app/controllers/Instructions.java
+++ app/controllers/Instructions.java
@@ -2,7 +2,7 @@ package controllers;
 
 import play.mvc.Controller;
 
-public class Application extends Controller {
+public class Instructions extends Controller {
 
     public static void index() {
         render();
